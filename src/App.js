711127370ced652e80425ae30f162a8efabe9fb5
Devin Lethbridge <devinbeth@gmail.com>
2018-05-14 20:44:42 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ class App extends Component {
     let breakdowns = projects.projects.sort((a, b) => a.added < b.added).map(e => {
       return (
         <div className='breakdown' key={e.added + e.startDate}>
-          <h5>Added {new Date(e.added).toLocaleDateString()} {new Date(e.added).toLocaleTimeString()}</h5>
-          <h2>{e.title}</h2>
+          <h4>Added {new Date(e.added).toLocaleDateString()} {new Date(e.added).toLocaleTimeString()}</h4>
+          <h1>{e.title}</h1>
           <h3>{e.type}</h3>
-          <h3>Casting Director {e.castingDirector}</h3>
+          <h3>Casting Director</h3> 
+          <h3>{e.castingDirector}</h3>
           <h4>Starts {new Date(e.startDate).toLocaleDateString()} {new Date(e.startDate).toLocaleTimeString()}</h4>
+          <h4>I'm interested <input id="checkBox" type="checkbox"/> </h4>
         </div>
       );
     })
     return (
       <div className="App">
-        {breakdowns}
+        <header>
+          <h2>Breakdowns</h2>
+        </header>
+        <div className='breakdownContainer'>
+          {breakdowns}
+        </div>
       </div>
     );
   }
